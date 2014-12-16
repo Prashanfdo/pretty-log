86b0251f237676f6bb21bb3564cc4d8698de676e
Prashan Fernando <prashanfdo@gmail.com>
2014-12-16 16:14:00 +0530

    changes added

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 require('colors');
+var logLevel = 1;
 
+exports.setLogLevel = function  (level) {
+	switch(level){
+		case 'info': 
+			logLevel = 1;
+			break;
+		case 'warn': 
+			logLevel = 2;
+			break;
+		case 'error': 
+			logLevel = 3;
+			break;
+		default: 
+			logLevel = 0;
+			break;
+	}
+}
 exports.error = function (msg) {
-  console.log('[ERROR] => '.red + msg)
+	if(logLevel >= 3){
+	  console.log('[ERROR] => '.red + msg);
+	}
 }
 
 exports.warn = function (msg) {
-  console.log('[WARN] => '.blue + msg)
+	if(logLevel >= 2){
+	  console.log('[WARN] => '.blue + msg)
+	}
 }
 
-exports.success = function (msg) {
-  console.log('[SUCCESS] => '.green + msg)
+exports.info = function (msg) {
+	if(logLevel >= 1){
+	  console.log(msg);
+	}
 }
 
 exports.debug = function (msg) {
-  console.log('[DEBUG] => '.yellow + msg)
+	if(logLevel >= 0){
+  		console.log('[DEBUG] => '.yellow + msg)
+	}
 }
 
